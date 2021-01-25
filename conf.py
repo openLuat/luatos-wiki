f1ede8fb3645672c2e4b486968eaf3a3ed8e039d
chenxuuu <lolicon@papapoi.com>
2021-01-25 22:47:55 +0800

    去掉recommonmark

--- conf.py
+++ conf.py
@@ -28,7 +28,7 @@ author = 'luatos'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'recommonmark',
+
 ]
 
 # Add any paths that contain templates here, relative to this directory.
