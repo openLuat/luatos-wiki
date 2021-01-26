0bd49136eaa7034dcbfb9ab228ce2649f5adb4d5
chenxuuu <lolicon@papapoi.com>
2021-01-26 18:29:22 +0800

    新窗口打开区域更正

--- _static/js/a.js
+++ _static/js/a.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     //新窗口打开链接
-    $(".content").find("a").each(function () {
+    $("article").find("a").each(function () {
         if ($(this).attr("target") !== "_blank") {
             $(this).attr('target', '_blank');
         }
