88cff547c1fa515e8ab3a0b25a0edfd3a07d58ab
chenxuuu <lolicon@papapoi.com>
2021-02-04 10:56:57 +0800

    统计代码

--- _static/luatos-emulator/js/luaTask.js
+++ _static/luatos-emulator/js/luaTask.js
@@ -178,3 +178,12 @@ function onSvgDivWheel() {
         svgScale -= 0.1;
         setSvgScale();
 };
+
+//百度统计代码
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?129c4539d58d0fff6a4695c52a5bcde5";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
