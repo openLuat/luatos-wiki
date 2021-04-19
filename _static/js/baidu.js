743ec390029f7b1cf7b8728b9a4a4ef900d2a55a
chenxuuu <lolicon@papapoi.com>
2021-04-19 10:18:44 +0800

    微调一下小问题

--- _static/js/baidu.js
+++ _static/js/baidu.js
@@ -2,6 +2,11 @@ var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
   hm.src = "https://hm.baidu.com/hm.js?129c4539d58d0fff6a4695c52a5bcde5";
-  var s = document.getElementsByTagName("script")[0]; 
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "6bo66yvxk8");
