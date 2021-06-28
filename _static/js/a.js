bafd918a63d319a37c1bdfcad919a6bf6525cbb7
chenxuuu <lolicon@papapoi.com>
2021-06-28 21:31:36 +0800

    模拟器在新窗口打开

--- _static/js/a.js
+++ _static/js/a.js
@@ -2,7 +2,7 @@ window.onload = function () {
     //新窗口打开链接
     $("article").find("a").each(function () {
         if ($(this).attr("href") != undefined &&
-            $(this).attr("href").indexOf("https://wiki.luatos.com") !== 0 &&
+            ($(this).attr("href").indexOf("https://wiki.luatos.com") !== 0 || $(this).attr("href").indexOf("https://wiki.luatos.com/_static/") == 0) &&
             $(this).attr("href").indexOf("#") !== 0 &&
             $(this).attr("target") !== "_blank") {
             $(this).attr('target', '_blank');
