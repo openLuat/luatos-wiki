9cf60d751012d520e55ca7f67089ba810bdcfa4b
Wendal Chen <wendal1985@gmail.com>
2022-07-21 00:20:07 +0800

    add: 添加自定义的搜索关键字

--- conf.py
+++ conf.py
@@ -80,6 +80,8 @@ html_css_files = [
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_search_options = {'dict': 'luatos-wiki/keyworks.txt'}
+
 
 html_show_sphinx = False
 html_search_language = 'zh'
