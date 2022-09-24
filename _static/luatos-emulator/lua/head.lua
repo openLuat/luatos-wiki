5203615df1173879caf126d7bc2202887b7c68e5
chenxuuu <lolicon@papapoi.com>
2022-09-24 11:23:47 +0800

    update: 加上没加的string库

--- _static/luatos-emulator/lua/head.lua
+++ _static/luatos-emulator/lua/head.lua
@@ -1,4 +1,5 @@
 local JSONLIB = require("JSON")
+utils = require("utils")
 
 --加强随机数随机性
 math.randomseed(tostring(os.time()):reverse():sub(1, 6))
@@ -17,6 +18,22 @@ json = {
         return JSONLIB:encode(t,nil,{null=json.null})
     end
 }
+--兼容部分5.1接口
+loadstring = load
+pack = {
+    pack = function ()
+        print("此环境为lua5.3，请使用string.pack代替pack.pack")
+    end,
+    unpack = function ()
+        print("此环境为lua5.3，请使用string.unpack代替pack.unpack")
+    end,
+}
+unpack = table.unpack
+BIT = require("bit")
+bit = BIT.bit32
+bit.bit = function(b) return bit.lshift(1,b) end
+bit.isset = function(v,p) return bit.rshift(v,p) % 2 == 1 end
+bit.isclear = function(v,p) return not bit.isset(v,p) end
 
 --触发请求的回调
 _G["@jsTriggerList"] = {}
